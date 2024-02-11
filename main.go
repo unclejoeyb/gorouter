ad1876c62832aaeeef38e1c2950e0e99d158d1d9
coscoaj1 <you@example.com>
2024-02-11 12:04:25 -0500

    4th commit

--- main.go
+++ main.go
@@ -4,37 +4,40 @@ import (
 	"net/http"
 )
 
-func baseHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/ninja":
-		switch r.Method {
-		case "GET":
-			NinjaHandlers.get(w, r)
-		case "POST":
-			NinjaHandlers.post(w, r)
-		case "PUT":
-			NinjaHandlers.put(w, r)
-		case "DELETE":
-			NinjaHandlers.delete(w, r)
-		}
-	case "/dojo":
-		switch r.Method {
-		case "GET":
-			DojoHandlers.get(w, r)
-		case "POST":
-			DojoHandlers.post(w, r)
-		case "PUT":
-			DojoHandlers.put(w, r)
-		case "DELETE":
-			DojoHandlers.delete(w, r)
-		}
-	default:
-		http.Error(w, "404 Not Found", http.StatusNotFound)
-	}
-}		
+// func baseHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/ninja":
+// 		switch r.Method {
+// 		case "GET":
+// 			NinjaHandlers.get(w, r)
+// 		case "POST":
+// 			NinjaHandlers.post(w, r)
+// 		case "PUT":
+// 			NinjaHandlers.put(w, r)
+// 		case "DELETE":
+// 			NinjaHandlers.delete(w, r)
+// 		}
+// 	case "/dojo":
+// 		switch r.Method {
+// 		case "GET":
+// 			DojoHandlers.get(w, r)
+// 		case "POST":
+// 			DojoHandlers.post(w, r)
+// 		case "PUT":
+// 			DojoHandlers.put(w, r)
+// 		case "DELETE":
+// 			DojoHandlers.delete(w, r)
+// 		}
+// 	default:
+// 		http.Error(w, "404 Not Found", http.StatusNotFound)
+// 	}
+// }
 
 func main() {
-	http.HandleFunc("/", baseHandler)
+	// http.HandleFunc("/", baseHandler)
+	http.HandleFunc("/helloWorld", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
 	http.ListenAndServe(":8080", nil)
 
 }
